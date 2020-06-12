b63a64fdca450b1b167e5a781203c4a4ddbfffc3
Alvaro Ferreira <alvarojfjunior@gmail.com>
2020-06-12 17:37:13 -0300

    first commit

---
+++ precache-manifest.1b11c30c5d56ca809a09daacdebb0f4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f35ef3dd7e292333f9c36e5b85ed948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67aa79c1bf48dd95a809",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "836a2c40dd4b48085292",
+    "url": "/static/css/main.4223cb3f.chunk.css"
+  },
+  {
+    "revision": "67aa79c1bf48dd95a809",
+    "url": "/static/js/2.b9a8442d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b9a8442d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "836a2c40dd4b48085292",
+    "url": "/static/js/main.e62fd2c7.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  }
+]);
